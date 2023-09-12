2b7cb90431f4b167a2481a41a406b6c47aa9cac1
Adam46072 <adam.bartl2001@seznam.cz>
2023-09-12 14:57:33 +0200

    Team complete

--- GameSimulation/Team.cs
+++ GameSimulation/Team.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace GameSimulation
 {
+    /// <summary>
+    /// Team is important for match between teams
+    /// </summary>
     internal class Team
     {
+        /// <summary>
+        /// Players in current team
+        /// </summary>
+        public IPlayer[] playersInTeam { get; set; }
+        public Team(IPlayer[] players)
+        {
+            this.playersInTeam = players;
+        }
     }
 }
