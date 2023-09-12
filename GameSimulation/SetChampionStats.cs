708c3438c1117907ab8a0a09dc79a4eae9a62c75
Adam46072 <adam.bartl2001@seznam.cz>
2023-09-12 14:54:57 +0200

    now has champion each player / bot stats

--- GameSimulation/SetChampionStats.cs
+++ GameSimulation/SetChampionStats.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace GameSimulation
 {
-    internal class SetChampionStats
+    internal class SetChampionStats<T>
     {
+        /// <summary>
+        ///  set hitpoints for chosen champion
+        /// </summary>
+        public int hitpoints { get; private set; }
+        /// <summary>
+        /// set attackDamage for chosen champion
+        /// </summary>
+        public int attackDamage { get; private set; }
+        /// <summary>
+        /// set resistance for chosen champion
+        /// </summary>
+        public int resistanceChampion { get; private set; }
+        public SetChampionStats(T gameChampion)
+        {
+            if (gameChampion.ToString().Equals("Mage"))
+            {
+                hitpoints = 180;
+                attackDamage = 70;
+                resistanceChampion = 30;
+            }
+            if (gameChampion.ToString().Equals("Warrior"))
+            {
+                hitpoints = 200;
+                attackDamage = 50;
+                resistanceChampion = 40;
+            }
+            if (gameChampion.ToString().Equals("Dwarf"))
+            {
+                hitpoints = 150;
+                attackDamage = 90;
+                resistanceChampion = 20;
+            }
+        }
     }
 }
